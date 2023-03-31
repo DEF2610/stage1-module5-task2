ad3f107c9d70b72bd94d56a4c335d3312943268c
d <maksimukart@mail.com>
2023-03-31 18:38:29 +0200

    d

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            List<Integer> integerList = new LinkedList<>();
+            for (Integer i : list) {
+                integerList.add(i / divider);
+            }
+            return integerList;
+        };
     }
 }
